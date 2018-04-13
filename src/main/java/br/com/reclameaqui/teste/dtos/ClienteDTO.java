42a632cfab3ddced4f2193add292a7b87cd4c915
Matheus Teles <teles.matheus@hotmail.com>
2018-04-13 20:39:59 -0300

    correcao

--- src/main/java/br/com/reclameaqui/teste/dtos/ClienteDTO.java
+++ src/main/java/br/com/reclameaqui/teste/dtos/ClienteDTO.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class ClienteDTO {
 
--- src/main/java/br/com/reclameaqui/teste/dtos/ReclamantesRequestDTO.java
+++ src/main/java/br/com/reclameaqui/teste/dtos/ReclamantesRequestDTO.java
@@ -3,11 +3,9 @@ package br.com.reclameaqui.teste.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class ReclamantesRequestDTO {
 
