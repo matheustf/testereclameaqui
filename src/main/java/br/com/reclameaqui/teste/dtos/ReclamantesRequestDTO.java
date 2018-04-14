815742aba7f0b5c2bc4757ca3fc406ebc68fdc79
Matheus Teles <teles.matheus@hotmail.com>
2018-04-13 21:02:00 -0300

    correcoes testes

--- src/main/java/br/com/reclameaqui/teste/dtos/ReclamantesRequestDTO.java
+++ src/main/java/br/com/reclameaqui/teste/dtos/ReclamantesRequestDTO.java
@@ -3,10 +3,12 @@ package br.com.reclameaqui.teste.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReclamantesRequestDTO {
 
 	private String empresa;
