d00b3bc7bb861fa27ad578f6aac6c6ca8220da98
Matheus Teles <teles.matheus@hotmail.com>
2018-04-16 00:17:09 -0300

    teste remocao classes de teste

--- src/test/java/br/com/reclameaqui/teste/SuiteApplicationTests.java
+++ src/test/java/br/com/reclameaqui/teste/SuiteApplicationTests.java
@@ -6,23 +6,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import br.com.reclameaqui.teste.controller.TestClienteController;
-import br.com.reclameaqui.teste.controller.TestReclamacaoController;
-import br.com.reclameaqui.teste.integracao.TestClienteIntegracao;
-import br.com.reclameaqui.teste.integracao.TesteApplicationTests;
 import br.com.reclameaqui.teste.service.TestClienteService;
 import br.com.reclameaqui.teste.service.TestReclamacaoService;
 import br.com.reclameaqui.teste.utils.TestUtils;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TestClienteController.class,
-        TestReclamacaoController.class,
         TestClienteService.class,
         TestReclamacaoService.class,
-        TestUtils.class,
-        TestClienteIntegracao.class,
-        TesteApplicationTests.class
+        TestUtils.class
 })
 public class SuiteApplicationTests {
     @Test
